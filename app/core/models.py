afb83bd92b2903f4f7f06ce099e516c33b05b8a8
Luis Juarez <manuel@kondosoft.com>
2019-01-10 11:58:38 -0600

    resolve flake8 linting

--- app/core/models.py
+++ app/core/models.py
@@ -8,13 +8,13 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """CREATES AND SAVES A NEW USER"""
         if not email:
-          raise ValueError('user must have an email address')
+            raise ValueError('user must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password=None):
 
         user = self.create_user(email, password)
